78d3a0f6c897d7b5829ebce941b1cb23ba9a9d9c
AnnaAmKh <aniykola@gmail.com>
2023-11-19 16:16:47 +0200

    first code hv #7

---
+++ clear_folder/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup, find_namespace_packages
+setup(
+    name='clear_folder',
+    version='0.0.1',
+    description='Very useful code',
+
+    packages=find_namespace_packages()
+)
