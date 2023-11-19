892147aec935dc05e364cea5470f07569507a427
AnnaAmKh <aniykola@gmail.com>
2023-11-19 16:32:41 +0200

    first code hv #7

--- clear_folder/setup.py
+++ clear_folder/setup.py
@@ -3,6 +3,9 @@ setup(
     name='clear_folder',
     version='0.0.1',
     description='Very useful code',
-
+    url='',
+    author='Anna Amelina',
+    author_email='author@gmail.com',
+    license='MIT',
     packages=find_namespace_packages()
 )
