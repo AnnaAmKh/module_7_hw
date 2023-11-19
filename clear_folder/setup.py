fdcb09090218640e7549f22bba08d3ae986b77ed
AnnaAmKh <143383921+AnnaAmKh@users.noreply.github.com>
2023-11-19 16:53:50 +0200

    Update setup.py

--- clear_folder/setup.py
+++ clear_folder/setup.py
@@ -1,9 +1,10 @@
 from setuptools import setup, find_namespace_packages
+
 setup(
     name='clear_folder',
     version='0.0.1',
     description='Very useful code',
-    url='',
+    url='https://github.com/AnnaAmKh/module_7_hw/tree/master/clear_folder',
     author='Anna Amelina',
     author_email='author@gmail.com',
     license='MIT',
